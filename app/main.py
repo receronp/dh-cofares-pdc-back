1bee0a6161321dbf193240d2d30d18a48aceefe8
Raul Ceron <receronp@gmail.com>
2024-11-15 19:06:57 +0100

    adds timer middleware

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 
 from .routers import users, chatbot
+from .middleware.timer import add_process_time_header  # Import the middleware function
 
 app = FastAPI()
 
@@ -9,6 +10,7 @@ origins = [
     "*",
 ]
 
+app.middleware("http")(add_process_time_header)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -20,9 +22,11 @@ app.add_middleware(
 app.include_router(users.router)
 app.include_router(chatbot.router)
 
+
 @app.get("/")
 async def root():
     return {"message": "Bienvenido a Cofares AI"}
+
 
 @app.get("/health")
 async def health():
